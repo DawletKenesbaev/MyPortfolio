0fb6cc5448fa4131969ba656ffd20b3a2d451d20
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-05-31 14:07:43 +0500

    kkk

--- src/Components/VideoDetails.jsx
+++ src/Components/VideoDetails.jsx
@@ -13,7 +13,7 @@ function VideoDetails() {
     const {id}=useParams();
   return (
   <div className={darkMode ? 'bg-neutral-800 text-white flex justify-center flex-row h-[calc(100%-56px)] ' : 'bg-neutral-100 text-gray-800  flex justify-center flex-row h-[calc(100%-56px)]'}>
-      <div className={darkMode ? 'w-full max-w-[1280px] flex flex-col lg:flex-row bg-neutral-800 text-white':'bg-neutral-100 text-gray-800 w-full max-w-[1280px] flex flex-col lg:flex-row'}>
+      <div className={darkMode ? 'w-full max-w-[1280px] flex flex-col lg:flex-row bg-neutral-800 text-white':'bg-neutral-100   w-full max-w-[1280px] flex flex-col lg:flex-row'}>
           <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto">
               <div className="h-[200px] p-2 md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
               <iframe   width={'100%'} height={'100%'} src={`https://www.youtube.com/embed/${id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
